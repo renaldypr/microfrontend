1e312b6a4719afd93827308cb896c6fa49da9829
Renaldy Pratama <renaldy@myboost.id>
2019-02-24 22:24:30 +0700

    add data fetching

--- list/src/App.js
+++ list/src/App.js
@@ -1,13 +1,80 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Alert, Table, Pagination, PaginationItem, PaginationLink } from 'reactstrap'
+import axios from 'axios';
 import './App.css';
-import {Clock} from "./Clock";
-import './styles/bootstrap/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  state = {
+    page: 0,
+    currentList: [],
+    lists: [],
+    error: false,
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({lists: res.data, currentList: res.data.slice(0, 10)})
+      })
+      .catch(err => {
+        this.setState({error: true})
+      })
+  }
+
+  onChangePage = (num) => () => {
+    this.setState({currentList: this.state.lists.slice((num-1) * 11, (num*11) - 1)})
+  }
+
   render() {
+    const paginationLength = Array.from({ length: 10 }, (v, i) => i);
+
     return (
       <div className="App">
-        <span>Welcome List</span>
+        <h1>List of Items with Pagination</h1>
+        {
+          this.state.error &&
+          <Alert color="danger">
+            API Error!
+          </Alert>
+        }
+        {
+          this.state.currentList.length ?
+          <Fragment>
+            <Table striped responsive>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>User ID</th>
+                  <th>Title</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.currentList.map((list) => (
+                    <tr>
+                      <td>{list.id}</td>
+                      <td>{list.userId}</td>
+                      <td>{list.title}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </Table>
+            <Pagination>
+              {
+                paginationLength.map(each => (
+                  <PaginationItem>
+                    <PaginationLink onClick={this.onChangePage(each+1)}>
+                      {each+1}
+                    </PaginationLink>
+                  </PaginationItem>
+                ))
+              }
+            </Pagination>
+          </Fragment>
+          : <span>Loading...</span>
+        }
       </div>
     );
   }
