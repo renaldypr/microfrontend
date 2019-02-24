557dae0907f87579f89a6f61d3f0974295561f72
Renaldy Pratama <renaldy@myboost.id>
2019-02-24 23:24:44 +0700

    add tab pages

--- nestedFragment/src/App.js
+++ nestedFragment/src/App.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
 import './App.css';
-import {Clock} from "./Clock";
-import './styles/bootstrap/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: '1',
+      name: null
+    };
+  }
+
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
   render() {
     return (
       <div className="App">
-        <span>Welcome Nested Fragment</span>
+        <h1 className="title">Content from Nested Service</h1>
+        <div>
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={this.state.activeTab === '1' ? 'active' : ''}
+                onClick={() => { this.toggle('1'); }}
+              >
+                Nested-1 Tab
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={this.state.activeTab === '2' ? 'active' : ''}
+                onClick={() => { this.toggle('2'); }}
+              >
+                Nested-2 Tab
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12">
+                  <div style={{padding: '20px'}}>
+                    <span>Hello from Nested Service</span>
+                  </div>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="12">
+                  <div style={{padding: '20px'}}>
+                    <span>Hello from Nested Service</span>
+                  </div>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
+        </div>
       </div>
     );
   }
